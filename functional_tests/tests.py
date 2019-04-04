6692f85179e1ef5212131e93eebca7576b655c86
hejp009 <hejunpeng2012@hotmail.com>
2019-04-04 20:14:38 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,46 +27,46 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
             
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Alice visits the website
-        self.browser.get(self.live_server_url)
+    # def test_can_start_a_list_and_retrieve_it_later(self):
+    #     # Alice visits the website
+    #     self.browser.get(self.live_server_url)
 
-        # Title and Header includes 'To-Do'
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+    #     # Title and Header includes 'To-Do'
+    #     self.assertIn('To-Do', self.browser.title)
+    #     header_text = self.browser.find_element_by_tag_name('h1').text
+    #     self.assertIn('To-Do', header_text)
 
-        # The web app invites her to a To-Do event
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+    #     # The web app invites her to a To-Do event
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a to-do item'
+    #     )
 
-        # She typed into a input bar the words: 'Buy peacock feathers'
-        inputbox.send_keys('Buy peacock feathers')
+    #     # She typed into a input bar the words: 'Buy peacock feathers'
+    #     inputbox.send_keys('Buy peacock feathers')
 
-        # The page updated when she pressed Enter
-        # The To-Do lists shows: '1. Buy peacock feathers'
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     # The page updated when she pressed Enter
+    #     # The To-Do lists shows: '1. Buy peacock feathers'
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # The page shows textbar again which can input other To-Dos
-        # She typed "Use peacock feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Make peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
+    #     # The page shows textbar again which can input other To-Dos
+    #     # She typed "Use peacock feathers to make a fly"
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Make peacock feathers to make a fly')
+    #     inputbox.send_keys(Keys.ENTER)
 
-        # The Page updates again, the lists shows two events
-        self.wait_for_row_in_list_table('2: Make peacock feathers to make a fly')
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        # She wants find out wheather this site can remember her lists
-        # She saw this site generates an unique url for her
-        # And the site has some texts to explain the function
+    #     # The Page updates again, the lists shows two events
+    #     self.wait_for_row_in_list_table('2: Make peacock feathers to make a fly')
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     # She wants find out wheather this site can remember her lists
+    #     # She saw this site generates an unique url for her
+    #     # And the site has some texts to explain the function
         
-        # self.fail('Finish the test!')
+    #     # self.fail('Finish the test!')
 
-        # She visits the site and the lists are there
+    #     # She visits the site and the lists are there
 
 
     def test_can_start_a_list_for_one_user(self):
@@ -146,3 +146,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        # Alice visits home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She saw the input box diplayed in the middle
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She create a new list and the inputbox still displayed in the middle
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
